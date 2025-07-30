128160051c9cac16cc31f9f68c009e53a44ccef3
arpitrai22 <workarpit02@gmail.com>
2025-07-31 00:12:14 +0530

    updated

--- operators.java
+++ operators.java
@@ -5,9 +5,53 @@ public static void main(String[] args) {
 int num1 = 7;
 int num2 = 5;
 
-int result = num1 + num2;
+int sum = num1 + num2;
 
-System.out.println(result);
+System.out.println(sum); 
+
+int mul = num1 * num2;
+
+System.out.println(mul);
+
+int sub = num1 - num2;
+
+System.out.println(sub);
+
+int div = num1 / num2;
+
+System.out.println(div);
+
+int mod = num1 % num2;
+
+System.out.println(mod);
+
+int inc = num1++;
+
+System.out.println(inc);
+
+int dec = num1--;
+
+System.out.println(dec);
+
+int andOp = num1 & num2;
+
+System.out.println(andOp);
+
+int orOp = num1 | num2;
+
+System.out.println(orOp);
+
+int xorOp = num1 ^ num2;
+
+System.out.println(xorOp);
+
+int leftshift = num1 << 2;
+
+System.out.println(leftshift);
+
+int rightshift = num1 >> num2;
+
+System.out.println(rightshift);
 
 }
 
