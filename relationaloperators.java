33cc4b0c3195dae7880457bf9e61e51f0ee5c87f
arpitrai22 <90815334+arpitrai22@users.noreply.github.com>
2025-07-31 22:08:20 +0530

    added

---
+++ relationaloperators.java
@@ -0,0 +1,13 @@
+public class relationaloperators {
+  
+   public static void main(String[] args) {
+
+    int a = 10;
+    int b = 20;
+
+    //Relational operators
+    System.out.println("is a = b: " + (a == b));  //false
+
+    
+   }
+}
