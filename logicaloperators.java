395814a203d53205319e3efcaba0748561012a0a
arpitrai22 <90815334+arpitrai22@users.noreply.github.com>
2025-07-31 22:30:05 +0530

    updated

---
+++ logicaloperators.java
@@ -0,0 +1,22 @@
+public class logicaloperators {
+  
+/**
+ * @param args
+ */
+public static void main(String args[]) {
+
+int a = 10;
+int b = 20;
+
+
+//Logical operators
+
+System.out.println((a == b) && (a < b));    //False
+System.out.println(( a== b) || (a <b ));    //True
+System.out.println(!(a == b) && ( a < b));   //True
+
+
+}
+
+
+}
