d6a6278443b1e618fb3a1bd4bc325d88febf5560
arpitrai22 <90815334+arpitrai22@users.noreply.github.com>
2025-08-01 00:55:09 +0530

    added

---
+++ SwitchExample.java
@@ -0,0 +1,40 @@
+public class SwitchExample {
+  
+public static void main(String[] args) {
+
+    // Example of a switch statement in Java
+    // This switch statement checks the day of the week and prints the corresponding name
+
+
+
+    int day = 3;        // Example day of the week (1-7)
+    
+    switch (day) {
+        case 1:
+            System.out.println("Monday");
+            break;
+        case 2:
+            System.out.println("Tuesday");
+            break;
+        case 3:
+            System.out.println("Wednesday");
+            break;
+        case 4:
+            System.out.println("Thursday");
+            break;
+        case 5:
+            System.out.println("Friday");
+            break;
+        case 6:
+            System.out.println("Saturday");
+            break;
+        case 7:
+            System.out.println("Sunday");
+            break;
+        default:
+            System.out.println("Invalid day");
+    }
+  } 
+
+
+}
