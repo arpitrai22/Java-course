f4d1b5c53feb45f44a193792e6e0d9267985624d
arpitrai22 <90815334+arpitrai22@users.noreply.github.com>
2025-08-03 00:31:24 +0530

    added

---
+++ DoWhile.java
@@ -0,0 +1,23 @@
+public class DoWhile {
+  
+    public static void main(String[] args) {
+
+        int i = 1;  // Initialize i to 1
+
+        // Do While loop executes at least once
+        // even if the condition is false initially
+
+        // Do-while loop that prints numbers from 1 to 5
+        do {
+
+            System.out.println(i);  // Print the current value of i
+
+            i++;  // Increment i by 1
+
+        } while (i >= 5);  // Loop will execute while i is less than or equal to 5
+
+
+    }
+
+
+}
