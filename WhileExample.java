e3090e0a0162f3d02441ad61ec45472d2eecee67
arpitrai22 <90815334+arpitrai22@users.noreply.github.com>
2025-08-02 23:46:34 +0530

    added

---
+++ WhileExample.java
@@ -0,0 +1,21 @@
+class WhileExample {
+
+
+    public static void main(String[] args) {
+
+        // Example of a while loop in Java
+        int i = 0;
+
+        // Loop until i is less than or equal to 5
+
+        while (i <= 5) {
+
+            System.out.println("i is: " + i);
+
+            // Increment i to avoid infinite loop
+            i++;
+
+
+        }
+    }
+}
